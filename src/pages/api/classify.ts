02e58139e37a530558c45e4d22c124be2d891f4a
tikisailor <40813246+tikisailor@users.noreply.github.com>
2024-11-16 22:51:37 +0800

    update classify

--- src/pages/api/classify.ts
+++ src/pages/api/classify.ts
@@ -1,7 +1,18 @@
 import { clusters } from "@/lib/classification";
-import { classify, EmbeddingSimilarityClassifier, generateObject, openai, zodSchema, jsonObjectPrompt, runTools } from "modelfusion";
-import { NextResponse } from "next/server";
+import { classify, EmbeddingSimilarityClassifier, generateObject, openai, zodSchema, jsonObjectPrompt, runTools, executeTool } from "modelfusion";
 import { z } from "zod";
+import { NextResponse } from "next/server";
+
+
+const gpt4Model = openai.ChatTextGenerator({
+  // explicit API configuration needed for NextJS environment
+  // (otherwise env variables are not available):
+  api: openai.Api({
+    apiKey: process.env.OPENAI_API_KEY,
+  }),
+  model: "gpt-4-1106-preview",
+  maxGenerationTokens: 1000,
+});
 
 export const config = { runtime: "edge" };
 
@@ -16,14 +27,15 @@ const requestSchema = z.array(messageSchema);
 
 export default async function classifyMessage (req: any, res: any) {
 
-    // if (req.method !== "POST") {
-    //     return new Response(
-    //     JSON.stringify({
-    //         error: `Method ${req.method} not allowed. Only POST allowed.`,
-    //     }),
-    //     { status: 405, headers: { "Content-Type": "application/json" } }
-    //     );
-    // }
+
+    if (req.method !== "POST") {
+        return new Response(
+        JSON.stringify({
+            error: `Method ${req.method} not allowed. Only POST allowed.`,
+        }),
+        { status: 405, headers: { "Content-Type": "application/json" } }
+        );
+    }
 
     const parsedData = requestSchema.safeParse(await req.json());
     console.log(parsedData)
@@ -42,140 +54,127 @@ export default async function classifyMessage (req: any, res: any) {
     const messages = parsedData.data;
     const lastMessage = messages[messages.length - 1];
 
-    // const classifier = new EmbeddingSimilarityClassifier({
-    //     embeddingModel: openai.TextEmbedder({
-    //       model: "text-embedding-ada-002",
-    //     }),
-    //     similarityThreshold: 0.82,
-    //     clusters: clusters
-    // });
+    const classifier = new EmbeddingSimilarityClassifier({
+        embeddingModel: openai.TextEmbedder({
+          model: "text-embedding-ada-002",
+        }),
+        similarityThreshold: 0.82,
+        clusters: clusters
+    });
 
-    // const result = await classify({
-    //     model: classifier,
-    //     value: lastMessage.content,
-    // });
+    const result = await classify({
+        model: classifier,
+        value: lastMessage.content,
+    });
 
-    // let classificationResult = null;
+    let classificationResult = null;
 
-    // switch (result) {
-    //     case "eth_transfer_to_address":
-    //         classificationResult = "eth_transfer_to_address";
-    //         break;
-    //     case "eth_transfer_to_ens":
-    //         classificationResult = "eth_transfer_to_ens"; 
-    //         break;
-    //     case "eth_transfer_to_basename":
-    //         classificationResult = "eth_transfer_to_basename";
-    //         break;
-    //     case "erc20_transfer_to_address":
-    //         classificationResult = "erc20_transfer_to_address";
-    //         break;
-    //     case "erc20_transfer_to_ens":
-    //         classificationResult = "erc20_transfer_to_ens";
-    //         break;
-    //     case "erc20_transfer_to_basename":
-    //         classificationResult = "erc20_transfer_to_basename";
-    //         break;
-    //     case "erc20_swap":
-    //         classificationResult = "erc20_swap";
-    //         break;
-    //     case "erc721_transfer":
-    //         classificationResult = "erc721_transfer";
-    //         break;
-    //     case "erc721_mint":
-    //         classificationResult = "erc721_mint";
-    //         break;
-    //     case null:
-    //         classificationResult = "null";
-    //         break;
-    // }
-
-    // console.log("Classification Result: ", classificationResult);
-
-    // const { text, toolResults } = await runTools({
-    //   model: openai.ChatTextGenerator({ model: "gpt-4-1106-preview" }),
-    //   tools: [tokenLookup, websearchTool],
-    //   prompt: [openai.ChatMessage.user(
-    //     "You are receiving a user input related to a ethereum transaction. " +
-    //     `The input has been classified as ${classificationResult}. ` +
-    //     "Use the available tokenLookup tool to find the token data. " +
-    //     "If in doubt, do a websearch to confirm the token name or symbol and choose the token with the biggest market cap." +
-    //     "Respond the JSON object of the correct token data."
-    //   )],
-    // });
-    
-    // if (text != null) {
-    //   console.log(`No result found`);
-    //   // return;
-    // } else {
-    //   console.log(`Result found: ${text}`);
-    // }
-
-    // console.log("Tool Results: ", toolResults);
-    
-    // for (const { tool, toolCall, args, ok, result } of toolResults ?? []) {
-    //   console.log(`Tool call:`, toolCall);
-    //   console.log(`Tool:`, tool);
-    //   console.log(`Arguments:`, args);
-    //   console.log(`Ok:`, ok);
-    //   console.log(`Result or Error:`, result);
-    // }
-
-    // const contractCall = await formatContractCall(lastMessage.content, classificationResult, text!);
-
-    // console.log("Contract Call: ", contractCall);
-
-    const testJson = {
-      role: "user",
-      content: "value",
+    switch (result) {
+        case "eth_transfer_to_address":
+            classificationResult = "eth_transfer_to_address";
+            break;
+        case "eth_transfer_to_ens":
+            classificationResult = "eth_transfer_to_ens"; 
+            break;
+        case "eth_transfer_to_basename":
+            classificationResult = "eth_transfer_to_basename";
+            break;
+        case "erc20_transfer_to_address":
+            // classificationResult = "erc20_transfer_to_address";
+            console.log(result);
+            const tokenDataResult = await extractTokenData(lastMessage.content, result);
+            console.log("Token Data Result: ", tokenDataResult);
+            const tokenData = tokenDataResult.tokenData[0];
+            console.log("Token Data: ", tokenData);
+            const tokens = await tokenList(tokenData.tokenSymbol, tokenData.tokenName);
+            console.log("Tokens: ", tokens);
+            const contractCall = await formatContractCall(lastMessage.content, result, tokens);
+            console.log("Contract Call: ", contractCall);
+            return NextResponse.json(contractCall);
+            break;
+        case "erc20_transfer_to_ens":
+            classificationResult = "erc20_transfer_to_ens";
+            break;
+        case "erc20_transfer_to_basename":
+            classificationResult = "erc20_transfer_to_basename";
+            break;
+        case "erc20_swap":
+            classificationResult = "erc20_swap";
+            break;
+        case "erc721_transfer":
+            classificationResult = "erc721_transfer";
+            break;
+        case "erc721_mint":
+            classificationResult = "erc721_mint";
+            break;
+        case null:
+          return NextResponse.json({contracts: null});
     }
-    return NextResponse.json(testJson);
-    // return new Response(JSON.stringify(testJson), {
-    //   status: 200,
-    //   headers: {
-    //     "Content-Type": "application/json"
-    //   }
-    // });
+
+    console.log("Classification Result: ", classificationResult);
+
+    // const tokens = await tokenList({ token_symbol: "DAI", token_name: "DAI Stablecoin" });
+
+    // console.log("Tokens: ", tokens);
+
+    const contractCall = await formatContractCall(lastMessage.content, classificationResult, "");
+
+    console.log("Contract Call: ", contractCall);
+
+    return NextResponse.json({});
+};
 
 
-    // return new Response(JSON.stringify(testJson), {
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //       "Cache-Control": "no-cache",
-    //       Connection: "keep-alive",
-    //       "Content-Encoding": "none",
-    //     },
-    // });
-}
+const formatContractCall = (messages: string, classificationResult: string, tokenData: string) =>
+  generateObject({
+    model: gpt4Model.asObjectGenerationModel(jsonObjectPrompt.text()),
 
+    schema: zodSchema(
+      z.object({
+        contracts: z.array(
+          z.object({
+            address: z.string().describe("The contract address"),
+            abi: z.string().describe("The contract abi"),
+            functionName: z.string().describe("The function name, probably e.g. `transfer` for erc20 token transfer"),
+            args: z.array(z.string()).describe("The function arguments, the required input parameters for the function"),
+          })
+        ),
+      })
+    ),
 
-// const gpt4Model = openai.ChatTextGenerator({
-//   // explicit API configuration needed for NextJS environment
-//   // (otherwise env variables are not available):
-//   api: openai.Api({
-//     apiKey: process.env.OPENAI_API_KEY,
-//   }),
-//   model: "gpt-4-1106-preview",
-//   maxGenerationTokens: 1000,
-// });
+    prompt:
+      `Based on this conversation ### ${messages} ### classified as ### ${classificationResult} ### and this token data ### ${tokenData} ###, formulate the transaction object for the coinbase sdk. Choose the most relevant token from the token list based on market capital.`
+});
 
-// const formatContractCall = (messages: string, classificationResult: string, tokenData: string) =>
-//   generateObject({
-//     model: gpt4Model.asObjectGenerationModel(jsonObjectPrompt.text()),
+const extractTokenData = (messages: string, classificationResult: string) =>
+  generateObject({
+    model: gpt4Model.asObjectGenerationModel(jsonObjectPrompt.text()),
 
-//     schema: zodSchema(
-//       z.object({
-//         contracts: z.array(
-//           z.object({
-//             address: z.string().describe("The contract address"),
-//             abi: z.string().describe("The contract abi"),
-//             functionName: z.string().describe("The function name, probably e.g. `transfer` for erc20 token transfer"),
-//             args: z.array(z.string()).describe("The function arguments, the required input parameters for the function"),
-//           })
-//         ),
-//       })
-//     ),
+    schema: zodSchema(
+      z.object({
+        tokenData: z.array(
+          z.object({
+            tokenName: z.string().describe("The erc20 token name the user wants to transfer"),
+            tokenSymbol: z.string().describe("the erc20 token symbol the user wants to transfer"),
+          })
+        ),
+      })
+    ),
 
-//     prompt:
-//       `Based on this conversation ### ${messages} ### classified as ### ${classificationResult} ### and this token data ### ${tokenData} ###, formulate the transaction object for the coinbase sdk`
-// });
+    prompt:
+      `Based on this conversation ### ${messages} ### classified as ### ${classificationResult} ### find all relevant token data the user has provided.`
+});
+
+const tokenList = async (token_symbol: string, token_name: string) => {
+  console.log("RUNNING TOKEN LOOKUP", token_symbol);
+  const response = await fetch(
+    `https://eth.blockscout.com/api/v2/tokens?q=${token_symbol}%20${token_name}&type=ERC-20`,
+    {
+      headers: {
+        'accept': 'application/json'
+      }
+    }
+  );
+  const data = await response.json();
+  return data;
+};
